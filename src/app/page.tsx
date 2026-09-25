7a6eb078df39acdf445b66785a1507349fbe7356
huss <samuelhussdb@gmail.com>
2026-09-25 10:22:46 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { campService } from "@/modules/camp/camp.service";
 import { LandingPage } from "@/components/landing/LandingPage";
 
+export const revalidate = 300;
+
 export default async function Home() {
   const camp = await campService.getPublicInfo().catch(() => null);
   return <LandingPage camp={camp} />;
